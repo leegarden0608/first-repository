7ebc0609c7a4a4c664998790668352dc100a3745
leegardeon0608 <leegarden0608@gmail.com>
2024-09-11 15:37:58 +0900

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,9 @@
 def print_hello():
     print("Hello World!!")
 
-if __name__=='__main__':
+def print_My_Name():
+    print("My name is Lee!")
+
+if __name__== '__main__':
+    print_My_Name()
     print_hello()
-    
