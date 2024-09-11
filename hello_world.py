1ebaa86e3ea22f43e9ff267727b86ba932a9812d
leegardeon0608 <leegarden0608@gmail.com>
2024-09-11 15:42:41 +0900

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -4,6 +4,11 @@ def print_hello():
 def print_My_Name():
     print("My name is Lee!")
 
+def add(x, y):
+    a = x + y
+    return a
+
 if __name__== '__main__':
     print_My_Name()
     print_hello()
+    add(36, 1290)
