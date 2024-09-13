1a72d86a4a2893ae92afdf5c1cd229f3abe0cb38
leegardeon0608 <leegarden0608@gmail.com>
2024-09-13 13:21:36 +0900

    Changed

--- prime_number.py
+++ prime_number.py
@@ -2,5 +2,10 @@ def is_prime_number(x):
     for num in range(2, x):
         if x % num == 0:
             print("The number {num1} is not a prime number.".format(num1=x))
+            print("hellow world\n")
+            print("let me go home\n")
+            print("let me go home\n")
+            print("let me go home\n")
+            print("let me go home\n")
             return
     print("The number {num1} is a prime number.".format(num1=x))
